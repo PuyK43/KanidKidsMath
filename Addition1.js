0b82a9ae7f83970109bcb15ae085d0bc9a49bfcc
PuyK43 <kochakorn93@gmail.com>
2024-02-09 23:32:42 +0700

    edit view Addition

---
+++ Addition1.js
@@ -0,0 +1,30 @@
+var Frontnum1 = document.getElementById("Frontnum1").innerHTML =
+Math.floor(Math.random() * 100);
+var behindnum1 =document.getElementById("behindnum1").innerHTML =
+Math.floor(Math.random() * 100);
+
+let Front_num1 = document.getElementById("Frontnum1").innerHTML;
+let behind_num1 = document.getElementById("behindnum1").innerHTML;
+let result1 = ( Front_num1+" + "+behind_num1+" = "+( parseInt(Front_num1) + parseInt(behind_num1) ) );
+console.log( Front_num1+" + "+behind_num1+" = "+( parseInt(Front_num1) + parseInt(behind_num1) ) );
+console.log(typeof Front_num1)
+
+function answer1() {
+    let result1 = ( Front_num1+" + "+behind_num1+" = "+( parseInt(Front_num1) + parseInt(behind_num1) ) );
+    document.getElementById("result1").innerHTML += "คำตอบที่ถูกต้อง " + result1 + "<br>";
+}
+
+function check1() {
+    let myanswer1 = document.getElementById('check1').innerHTML = document.getElementById('myanswer1').value;
+    if(parseInt(Front_num1) + parseInt(behind_num1) == myanswer1){
+        document.getElementById("check1").innerHTML = "ถูกต้อง เก่งมาก"
+    }else if(myanswer1 == ""){
+        document.getElementById("check1").innerHTML = "กรุณาใส่คำตอบ"
+    }else{
+        document.getElementById("check1").innerHTML = "พยายามอีกนิดนะ"
+        //console.log(typeof myanswer)
+        //console.log(myanswer)
+        //console.log(typeof parseInt(Front_num) + parseInt(behind_num))
+        //console.log(parseInt(Front_num) + parseInt(behind_num))
+    }
+}
